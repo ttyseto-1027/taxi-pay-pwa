1d2431707a73df6739c7399590ef91b9703dd195
ttyseto-1027 <tty.seto1027@gmail.com>
2026-07-14 17:05:03 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 // Firebase コンソールで「ウェブアプリ」を登録した後、この値を置き換えてください。
 // apiKey は公開されることを前提とした識別情報です。安全性は Firestore Security Rules で確保します。
 window.TAXI_PAY_FIREBASE_CONFIG = {
-  enabled: false,
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.firebasestorage.app",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  enabled: true,
+  apiKey: "AIzaSyAJ_Q9w5v6In96Jga_oUKQd4BHTYAMNRPw",
+  authDomain: "taxipayrollprocessingsimulator.firebaseapp.com",
+  projectId: "taxipayrollprocessingsimulator",
+  storageBucket: "taxipayrollprocessingsimulator.firebasestorage.app",
+  messagingSenderId: "801275735645",
+  appId: "1:801275735645:web:c40f018f04c6d53cf4a7b4",
+  measurementId: "G-3QHZT4CXS3"
 };
